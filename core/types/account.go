0caf15f4816f12b139d2f53aa3308eee71c66c85
Wody <wody@firstchain.co>
2019-07-02 22:08:54 +0900

    add marshaler to account and transaction

--- core/types/account.go
+++ core/types/account.go
@@ -1,11 +1,14 @@
 package types
 
 import (
+	"encoding/json"
+
 	"github.com/fletaio/fleta/common"
 )
 
 // Account is a interface that defines common account functions
 type Account interface {
+	json.Marshaler
 	Address() common.Address
 	Name() string
 	Clone() Account
--- core/types/transaction.go
+++ core/types/transaction.go
@@ -1,12 +1,15 @@
 package types
 
 import (
+	"encoding/json"
+
 	"github.com/fletaio/fleta/common"
 	"github.com/fletaio/fleta/common/amount"
 )
 
 // Transaction is an interface that defines common transaction functions
 type Transaction interface {
+	json.Marshaler
 	Timestamp() uint64
 	Fee(loader LoaderProcess) *amount.Amount
 	Validate(loader LoaderProcess, signers []common.PublicHash) error
