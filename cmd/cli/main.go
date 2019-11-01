00847e881836a96d75a50c618d339ba15ca0443b
Wody <wody@firstchain.co>
2019-11-01 12:05:59 +0900

    use default port

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	var hostURL string
 	var rootCmd = &cobra.Command{Use: "cli"}
-	rootCmd.PersistentFlags().StringVar(&hostURL, "host", "http://localhost:58001", "url of the node to access")
+	rootCmd.PersistentFlags().StringVar(&hostURL, "host", "http://localhost:58000", "url of the node to access")
 	rootCmd.AddCommand(keyCommand(&hostURL))
 	rootCmd.AddCommand(accountCommand(&hostURL))
 	rootCmd.AddCommand(txCommand(&hostURL))
