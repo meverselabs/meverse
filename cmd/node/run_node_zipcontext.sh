a0a929bb4b721f1aebbc345498d2049d52eee720
Sam <judasowelu@gmail.com>
2022-05-24 16:55:21 +0900

    update zipcontext script

--- cmd/node/run_node_zipcontext.sh
+++ cmd/node/run_node_zipcontext.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/meverselabs/meverse
-cd ./meverse/cmd/node
+# git clone https://github.com/meverselabs/meverse
+# cd ./meverse/cmd/node
 wget https://rpc.meversemainnet.io/zipcontext
 unzip zipcontext
 mv ./data ./ndata
@@ -8,7 +8,7 @@ sed -e "s/InitHeight = 0/$(sed '2!d' _config.toml)/" config.toml > config.toml_ 
 sed -e "s/InitHash = \"\"/$(sed '3!d' _config.toml)/" config.toml > config.toml_ && mv config.toml_ config.toml
 sed -e "s/InitTimestamp = 0/$(sed '4!d' _config.toml)/" config.toml > config.toml_ && mv config.toml_ config.toml
 # sed -e "s/RPCPort = 8541/RPCPort = 8541/" config.toml > config.toml_ && mv config.toml_ config.toml
-go build
 rm -r _config.toml
 rm -r zipcontext
+go build
 ./node
