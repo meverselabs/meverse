705bb6f08c5837a0e77af88bab558878d8680420
Sam <judasowelu@gmail.com>
2023-03-14 14:37:28 +0900

    add default evm debugmod

---
+++ ethereum/params/meverse_params.go
@@ -0,0 +1,8 @@
+//no change go-ethereum 1.10.17.stable
+
+package params
+
+const (
+	BlockGasLimit uint64 = 5000000 // estimate gas maximum value
+	EvmDebug             = false   // evm tracer enabble
+)
